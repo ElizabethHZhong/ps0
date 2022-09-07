1dc398086f1a6671de54be47014a49477d5e7928
ElizabethHZhong <89757780+ElizabethHZhong@users.noreply.github.com>
2022-09-06 21:23:45 -0400

    update

--- fall2022/psets/ps0/ps0.py
+++ fall2022/psets/ps0/ps0.py
@@ -8,6 +8,9 @@
 #
 # Setup
 #
+from shutil import register_unpack_format
+
+
 class BinaryTree:
     def __init__(self, root):
         self.root: BTvertex = root
@@ -30,15 +33,21 @@ class BTvertex:
 # ... tree rooted at vertex v to the size of that subtree
 # Runtime: O(n)
 def calculate_sizes(v):
+    # check if v is None
     if not v:
         return 0
-    
+
+    # base case: leaf node, where size is 1
     if not v.left and not v.right:
         v.size = 1
+
+    # recursive call: vertex size is the sum of children + 1
     else:
-        v.size = calculate_sizes(v.left) + calculate_sizes(v.right)
+        v.size = calculate_sizes(v.left) + calculate_sizes(v.right) + 1
     
+    # return the size of the vertex
     return v.size
+
 
 #
 # Problem 1c
@@ -49,6 +58,40 @@ def calculate_sizes(v):
 # Output: A BTvertex that, if removed from the tree, would result
 # ... in disjoint trees that all have at most n/2 vertices
 # Runtime: O(h)
-def find_vertex(r): 
-    # Your code goes here
-    pass
+
+def find_vertex(r):
+    # check for r is None
+    if not r:
+        return r
+    
+    # store n/2 in a variable
+    half = r.size / 2
+    
+    # both children exist
+    if r.left and r.right:
+        # base case: return the vertex if both child trees <= n/2
+        if r.left.size <= half and r.right.size <= half:
+            return r
+
+        # reduce the potential function by moving to the larger child node
+        elif r.left.size < r.right.size:
+            return find_vertex(r.right)
+        else:
+            return find_vertex(r.left)
+
+    # only left child exists
+    elif r.left and not r.right:
+        # base case: return the vertex if the child tree <= n/2
+        if r.left.size <= half:
+            return r
+        # reduce the potential function by moving to the child node
+        return find_vertex(r.left)
+
+    # only right child exists
+    elif not r.left and r.right:
+        # base case: return the vertex if the child tree <= n/2
+        if r.right.size <= half:
+            return r
+        # reduce the potential function by moving to the child node
+        return find_vertex(r.right)
+
